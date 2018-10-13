9c57015367c96032a4d24656ef45cb537b574fb3
KMoesgaardHome <kmo@c2it.dk>
2018-10-13 10:46:31 +0200

    First dev Commit

---
+++ DatabaseContext/Migrations/201810121156427_M1.cs
@@ -0,0 +1,26 @@
+namespace DatabaseContext.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class M1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Events",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        EventName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Events");
+        }
+    }
+}
---
+++ Model/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Event
+    {
+        public string EventName { get; set; }
+        [Key]
+        public int Id { get; set; }
+    }
+}
