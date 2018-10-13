2762312d42d0a73758b91384097dfd2d7c9b76c3
KMoesgaardHome <kmo@c2it.dk>
2018-10-13 10:44:54 +0200

    first commit

---
+++ DatabaseContext/TSWContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseContext
+{
+    public class TSWContext : DbContext
+    {
+        public TSWContext() : base("TSWContext")
+        {
+            
+        }
+        public DbSet<Model.Event> Events { get; set; }
+    }
+}
